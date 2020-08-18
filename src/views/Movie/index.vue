f2169d7f5fe8a978294554edccea20e3f72fad0c
abcdyiandwuyanxi <2793038039@qq.com>
2020-08-18 17:37:56 +0800

    add createComponents

--- src/views/Movie/index.vue
+++ src/views/Movie/index.vue
@@ -9,7 +9,7 @@
 					<span>大连</span><i class="iconfont icon-lower-triangle"></i>
 				</router-link>
 				<div class="hot_swtich">
-					<router-link tag="div" to="/movie/nowPlaying" class="hot_item">正在热映</router-link>
+					<router-link tag="div" to="/movie/nowPlaying" class="hot_item">热映中</router-link>
 					<router-link tag="div" to="/movie/comingSoon" class="hot_item">即将上映</router-link>
 				</div>
 				<router-link tag="div" to="/movie/search" class="search_entry">
