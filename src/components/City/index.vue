9a0d3302d2d493e54d9d3e49247deb0b62ccb264
abcdyiandwuyanxi <2793038039@qq.com>
2020-11-01 23:58:24 +0800

    add getCity

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="city_body">
     <div class="city_list">
-      <div class="city_hot">
-        <h2>热门城市</h2>
-        <ul class="clearfix">
-          <li v-for="item in hotList" :key="item.id">{{item.nm}}</li>
-        </ul>
-      </div>
-      <div class="city_sort" ref="city_sort">
-        <div v-for="item in cityList" :key="item.index">
-          <h2>{{item.index}}</h2>
-          <ul>
-            <li v-for="itemList in item.list" :key="itemList.id">{{itemList.nm}}</li>
-          </ul>
-        </div>
-      </div>
+      <Loading v-if="isLoading" />
+      <Scroller v-else ref="city_List">
+        <div>
+          <div class="city_hot">
+            <h2>热门城市</h2>
+            <ul class="clearfix">
+              <li v-for="item in hotList" :key="item.id" @click="handleToCity(item.nm , item.id)">{{ item.nm }}</li>
+            </ul>
+          </div>
+          <div class="city_sort" ref="city_sort">
+            <div v-for="item in cityList" :key="item.index">
+              <h2>{{ item.index }}</h2>
+              <ul>
+                <li v-for="itemList in item.list" :key="itemList.id" @click="handleToCity(itemList.nm , itemList.id)">{{ itemList.nm }}</li>
+              </ul>
+            </div>
+          </div>
+       </div>
+      </Scroller>
     </div>
     <div class="city_index">
       <ul>
-        <li
-          v-for="(item,index) in cityList"
-          :key="item.index"
-          @touchstart="handleToIndex(index)"
-        >{{item.index}}</li>
+        <li v-for="(item, index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{ item.index }}</li>
       </ul>
     </div>
   </div>
@@ -35,20 +36,35 @@ export default {
     return {
       cityList: [],
       hotList: [],
-    };
+      isLoading : true
+    }
   },
-  // mounted是vue一个钩子函数，在这发起后端请求，拿回数据，配合路由钩子做一些事情
-  mounted() {
-    this.axios.get("/api/cityList").then((res) => {
-      var msg = res.data.msg;
-      if (msg == "ok") {
-        var cities = res.data.data.cities;
-        var { cityList, hotList } = this.formatCityList(cities);
-        this.cityList = cityList;
-        this.hotList = hotList;
-      }
-    });
-  },
+  mounted(){
+
+        var cityList = window.localStorage.getItem('cityList');
+        var hotList = window.localStorage.getItem('hotList');
+
+        if(cityList && hotList){
+            this.cityList = JSON.parse(cityList);
+            this.hotList = JSON.parse(hotList);
+            this.isLoading = false;
+        }
+        else{
+            this.axios.get('/api/cityList').then((res)=>{
+                var msg = res.data.msg;
+                if(msg === 'ok'){
+                    this.isLoading = false;
+                    var cities = res.data.data.cities;
+                    //[ { index : 'A' , list : [{ nm : '阿城' , id : 123 }] } ]
+                    var { cityList , hotList } = this.formatCityList(cities);
+                    this.cityList = cityList;
+                    this.hotList = hotList;
+                    window.localStorage.setItem('cityList' , JSON.stringify(cityList));
+                    window.localStorage.setItem('hotList' , JSON.stringify(hotList));
+                }
+            });
+        }
+    },
   methods: {
     formatCityList(cities) {
       var cityList = [];
@@ -63,11 +79,9 @@ export default {
       for (var i = 0; i < cities.length; i++) {
         var firstLetter = cities[i].py.substring(0, 1).toUpperCase();
         if (toCom(firstLetter)) {
-          cityList.push({
-            index: firstLetter,
-            list: [{ nm: cities[i].nm, id: cities[i].id }],
-          });
-        } else {
+          cityList.push({ index: firstLetter,list: [{ nm: cities[i].nm, id: cities[i].id }] });
+        } 
+        else {
           for (var j = 0; j < cityList.length; j++) {
             if (cityList[j].index === firstLetter) {
               cityList[j].list.push({ nm: cities[i].nm, id: cities[i].id });
@@ -81,7 +95,8 @@ export default {
           return 1;
         } else if (n1.index < n2.index) {
           return -1;
-        } else {
+        } 
+        else {
           return 0;
         }
       });
@@ -100,9 +115,17 @@ export default {
       };
     },
     handleToIndex(index) {
-      var h2 = this.$refs.city_sort.getElementsByTagName('h2');
-      this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
-    }
+      var h2 = this.$refs.city_sort.getElementsByTagName("h2");
+      // this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+      this.$refs.city_List.toScrollTop(-h2[index].offsetTop);
+    },
+    handleToCity(nm,id){
+            this.$store.commit('city/CITY_INFO',{ nm , id });
+            console.log('111');
+            window.localStorage.setItem('nowNm',nm);
+            window.localStorage.setItem('nowId',id);
+            this.$router.push('/movie/nowPlaying');
+        }
   }
 }
 </script>
--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -1,24 +1,23 @@
 <template>
   <div class="movie_body">
+<Loading v-if="isLoading" />
+ <Scroller v-else>
     <ul>
+      <li class="pullDown">{{ pullDownMSG }}</li>
       <li v-for="item in comingList" :key="item.id">
-        <div class="pic_show">
-          <img :src="item.img | setWH('128.180')">
+        <div class="pic_show" @click="handleToDetail"><img :src="item.img | setWH('128.180')" />
         </div>
         <div class="info_list">
-          <h2>
-            {{item.nm}}
-            <img v-if="item.version" src="@/assets/maxs.png" alt="">
-          </h2>
+          <h2>{{ item.nm }}<img v-if="item.version" src="@/assets/maxs.png" alt="" /></h2>
           <p>
-            <span class="person">{{item.wish}}</span> 人想看
+            <span class="person">{{ item.wish }}</span> 人想看
           </p>
-          <p>主演: {{item.star}}</p>
-          <p>{{item.rt}}上映</p>
+          <p>主演: {{ item.star }}</p>
+          <p>{{ item.rt }}上映</p>
         </div>
-        <!-- <div class="btn_pre">预售</div> -->
       </li>
     </ul>
+</Scroller>
   </div>
 </template>
 
@@ -27,16 +26,30 @@ export default {
   name: "ComingSoon",
   data() {
     return {
-      comingList: []
+      comingList: [],
+      isLoading : true,
+      pullDownMSG: "",
+      prevCityId : -1
     };
   },
-  mounted() {
-    this.axios.get("/api/movieComingList?cityId=10").then((res) => {
+  activated() {
+    var cityId = this.$store.state.city.id;
+    if( this.prevCityId === cityId ){ return; }
+    this.isLoading = true;
+    this.axios.get('/api/movieComingList?cityId='+cityId).then((res) => {
       var msg = res.data.msg;
       if (msg === "ok") {
         this.comingList = res.data.data.comingList;
+        this.isLoading = false;
+        this.prevCityId = cityId;
       }
     });
+  },
+
+  methods: {
+    handleToDetail() {
+      console.log("handleToDetail");
+    },
   },
 };
 </script>
